dfce2c9abff7cf8bce26b28b5afbf34a70ad6d75
gbaltzis16-ctrl <gbaltzis16@gmail.com>
2026-02-22 10:52:20 -0500

    name entry

--- server.py
+++ server.py
@@ -1,22 +1,43 @@
 import os
-
 import websockets
-import websockets as ws
 import asyncio
 
 PORT = int(os.environ.get("PORT", 8000))
 
-clients = set()
+clients = {} #{ websocket: username }
 
 async def handler(websocket ):
-    clients.add(websocket)
     try:
+        username = (await websocket.recv()).strip()
+
+        if not username:
+            await websocket.close()
+            return
+
+        clients[websocket] = username
+
+        join_Msg=(f"* {username} joined *")
+        for client in clients:
+            if client != websocket:
+                await client.send(join_Msg)
+
         async for message in websocket:
+            message = message.strip()
+            if not message:
+                continue
+
+            message=f"{username}: {message}"
+
             for client in clients:
                 if client != websocket:
                     await client.send(message)
     finally:
-        clients.remove(websocket)
+        if websocket in clients:
+            username = clients.pop(websocket)
+            leave_Msg= f"* {username} left *"
+            for client in clients:
+                if client != websocket:
+                    await client.send(leave_Msg)
 
 async def main():
     async with websockets.serve(handler, "0.0.0.0", PORT):
